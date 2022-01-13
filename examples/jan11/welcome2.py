9a72a739aa4c8f15331be8b5c11098e03a638f43
AlexandraWheeler <97530213+AlexandraWheeler@users.noreply.github.com>
2022-01-13 09:23:52 -0500

    Add files via upload

---
+++ examples/jan11/welcome2.py
@@ -0,0 +1,15 @@
+first_name = input("First name: ")
+last_name = input("Last name: ")
+print(f"Welcome {first_name} {last_name}!")
+
+print("""Welcome to my store!
+Please enter 3 items you would like: """)
+first_item = input("First_item: ")
+second_item = input("Second item: ")
+third_item = input("Third item: ")
+print(f"""***Receipt:***
+You ordered:
+-{first_item}
+-{second_item}
+-{third_item}
+Goodbye!""")
